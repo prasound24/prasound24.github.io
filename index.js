1dcaa3ecba18d5f1616330a83a6f13d685a559c3
dev <>
2025-02-19 14:17:11 -0800

    Simplified the main layout

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-import { DB } from './lib/utils.js';
+import { DB, $, $$ } from './lib/utils.js';
 import * as base from './create/base.js';
-
-let $ = x => document.querySelector(x);
-let $$ = x => document.querySelectorAll(x);
-
-$('#tabs').onclick = e => {
-  let tab = e.target.getAttribute('tab');
-  if (!tab) return;
-
-  for (let g of $$('#examples .grid'))
-    g.classList.toggle('active', tab == g.id);
-  for (let t of $$('#tabs span'))
-    t.classList.toggle('active', tab == t.getAttribute('tab'));
-};
 
 for (let a of $$('.grid > a')) {
   let img = a.querySelector('img');
@@ -41,7 +28,4 @@ async function initGallery() {
     a.append(img);
     grid.append(a);
   }
-
-  if (sids.length > 0)
-    $('[tab="others"]').style.display = '';
 }
